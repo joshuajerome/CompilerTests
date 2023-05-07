377a1dc6a32ce0f735cfac7c602c111b63407119
Joshua Jerome <joshua.jerome@gmail.com>
2023-05-07 16:31:07 -0700

    added neighbors.c and .err

---
+++ project3/examples/neighbors.c
@@ -0,0 +1,45 @@
+int x;
+char *p;
+char b[10];
+int ** q;
+char *b[10];
+char (*c)[10];
+int **d[10];
+int *(*e)[10];
+
+char (*b)[10];
+char *c[10];
+int *(*d)[10];
+int **e[10];
+
+int f();
+char *g(); 
+char a()[10];
+int b[10]();
+int **h();
+char (*p)();
+int *(*q)();
+
+
+
+
+
+
+int x, a[10];
+
+int f(int x, int y) {
+
+	int y;
+	{
+		int w;
+		w = w + z;
+		y = g();
+	}
+
+	w = a[x];
+}
+
+int x, a[5];
+int f(int x) {}
+
+
