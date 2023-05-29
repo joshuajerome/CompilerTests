024d29dc96e2edd97f0c798a4bf287e3e69e7970
Joshua Jerome <joshua.jerome@gmail.com>
2023-05-28 23:21:11 -0700

    updated .c files for trees

--- project5/tree2.c
+++ project5/tree2.c
@@ -1,40 +1,39 @@
 /*
- * tree.c, or I've always thought structures were overrated, didn't you?
- *
- * Structures?  We ain't got no structures.  We don't need no structures.
- * I don't have to show you any stinkin' structures!
- *
- * Believe it or not, your compiler will be able to generate assembly code
- * for this program.  Scary, huh?
+ * tree2.c, or is it any better with function pointers?
  */
 
 int printf(), *malloc(), *null;
 
-int **insert(int **root, int *data)
+int **insert(int **root, int *data, int cmp())
 {
     if (!root) {
 	root = (int **) malloc(sizeof(*root) * 3);
 	root[0] = data;
 	root[1] = null;
 	root[2] = null;
-    } else if (data < root[0]) {
-	root[1] = (int *) insert((int **) root[1], data);
-    } else if (data > root[0])
-	root[2] = (int *) insert((int **) root[2], data);
+    } else if (cmp(data, root[0]) < 0) {
+	root[1] = (int *) insert((int **) root[1], data, cmp);
+    } else if (cmp(data, root[0]) > 0)
+	root[2] = (int *) insert((int **) root[2], data, cmp);
 
     return root;
 }
 
-int search(int **root, int *data)
+int search(int **root, int *data, int (*cmp)())
 {
+    int diff;
+
+
     if (!root)
 	return 0;
 
-    if (data < root[0])
-	return search((int **) root[1], data);
+    diff = (*cmp)(data, root[0]);
 
-    if (data > root[0])
-	return search((int **) root[2], data);
+    if (diff < 0)
+	return search((int **) root[1], data, cmp);
+
+    if (diff > 0)
+	return search((int **) root[2], data, cmp);
 
     return 1;
 }
@@ -57,6 +56,11 @@ int inorder(int **root)
     }
 }
 
+int compare(int *x, int *y)
+{
+    return *x - *y;
+}
+
 int main(void)
 {
     int **root;
@@ -70,14 +74,14 @@ int main(void)
     }
 
     root = (int **) null;
-    root = insert(root, &a[7]);
-    root = insert(root, &a[4]);
-    root = insert(root, &a[1]);
-    root = insert(root, &a[0]);
-    root = insert(root, &a[5]);
-    root = insert(root, &a[2]);
-    root = insert(root, &a[3]);
-    root = insert(root, &a[6]);
+    root = insert(root, &a[7], &compare);
+    root = insert(root, &a[4], &compare);
+    root = insert(root, &a[1], &compare);
+    root = insert(root, &a[0], &compare);
+    root = insert(root, &a[5], compare);
+    root = insert(root, &a[2], compare);
+    root = insert(root, &a[3], compare);
+    root = insert(root, &a[6], compare);
     printf("preorder traversal:\n");
     preorder(root);
     printf("inorder traversal:\n");
